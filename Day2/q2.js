027bff18e53f297c9e7b66133b534b2f66af85cb
SaurabhWhadgar <50226745+SaurabhWhadgar@users.noreply.github.com>
2020-07-23 22:05:53 +0530

    Add files via upload

---
+++ Day2/q2.js
@@ -0,0 +1,5 @@
+var str = "We are, learning Javascript";
+var res = str.substring(7, 13);
+var strsplit = str.splice(
+var n = str.replace("are", "");
+var splicedstr = str.split(",");  
---
+++ Day2/q3.js
@@ -0,0 +1,8 @@
+let age = confirm("Are you over 21")
+console.log(age)
+
+if(age){
+    console.log("Lets Drink")
+}else{
+    console.log("Not to Drink")
+}
